1f2d5dbde61e54d735cfabb3b8b693a64bc20ed1
Eric <eric20001210@gmail.com>
2022-05-13 08:33:08 +1000

    implemented rgb drawing feature

--- application.js
+++ application.js
@@ -16,4 +16,31 @@ for (let i = 0; i < squareLength; ++i) {
     board.appendChild(rows);
 }
 
+// Generates a random color and returns its corresponding string representation
+function randomColor() {
+    let red, green, blue;
+    let colorRep;
+    red = Math.floor(Math.random() * 256);
+    green = Math.floor(Math.random() * 256);
+    blue = Math.floor(Math.random() * 256);
+    colorRep = "rgb(" + red + ", " + green + ", " + blue + ")";
+    return colorRep;
+}
+
+// Colors the grid (to an random rgb color)
+function color_grid(grid) {
+    grid.style.backgroundColor = randomColor();
+}
+
+
+const grids = document.querySelectorAll(".board > .rows > .grid");
+
+grids.forEach((grid) => {
+
+    grid.addEventListener('mouseenter', function(e) {
+        color_grid(grid);
+        e.stopPropagation();
+    })
+})
+
 
