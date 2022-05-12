1f0fb745b76ed7d17536746f2b1b43689686c58d
Eric <eric20001210@gmail.com>
2022-05-13 09:55:33 +1000

    Implemented color mode for drawing

--- application.js
+++ application.js
@@ -2,7 +2,7 @@ let squareLength = 16;
 
 const body = document.querySelector("body");
 const board = document.querySelector(".board");
-
+let rainbowOn = false;
 
 for (let i = 0; i < squareLength; ++i) {
     const rows = document.createElement('div');
@@ -28,19 +28,38 @@ function randomColor() {
 }
 
 // Colors the grid (to an random rgb color)
-function color_grid(grid) {
+function color_grid_rainbow(grid) {
     grid.style.backgroundColor = randomColor();
 }
-
+function color_grid_black(grid) {
+    grid.style.backgroundColor = "black";
+}
 
 const grids = document.querySelectorAll(".board > .rows > .grid");
+const buttonBlack = document.querySelector("button.black");
+const buttonRainbow =  document.querySelector("button.rainbow");
+const resize = document.querySelector(".sizes > button")
+
+buttonRainbow.addEventListener('click', function(e) {
+    rainbowOn = true;
+    e.stopPropagation();
+});
+
+buttonBlack.addEventListener('click', function(e) {
+    rainbowOn = false;
+    e.stopPropagation();
+});
 
 grids.forEach((grid) => {
-
     grid.addEventListener('mouseenter', function(e) {
-        color_grid(grid);
+        if (rainbowOn) {
+            color_grid_rainbow(grid);
+        } else {
+            color_grid_black(grid);
+        }
         e.stopPropagation();
     }, {capture: false, once:true});
-})
+});
+
 
 
